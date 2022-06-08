58796ce8d1f8498cbc8f3a42b2e25077b6a7c42a
Dylan Davis <djdavis1420@gmail.com>
2022-06-08 18:04:25 -0500

    More Accurate Test Names

--- test/classes/albums/sync_test.rb
+++ test/classes/albums/sync_test.rb
@@ -3,15 +3,15 @@
 require 'test_helper'
 
 class SyncTest < ActiveSupport::TestCase
-  test 'returns array of photos when called with valid album id' do
+  test 'returns array of photos when input is existing album id' do
     assert_not_empty Albums::Sync.call(42)
   end
 
-  test 'returns empty array when called with invalid album id' do
+  test 'returns empty array when input is nonexisting album id' do
     assert_empty Albums::Sync.call(-42)
   end
 
-  test 'raises argument error when input is not a number' do
+  test 'raises argument error when input is not an integer' do
     assert_raises(ArgumentError) { Albums::Sync.call('foo') }
   end
 end
