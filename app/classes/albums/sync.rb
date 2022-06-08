fb55c3728c93f24e135900f994a1ab96c929eebb
Dylan Davis <djdavis1420@gmail.com>
2022-06-08 18:03:03 -0500

    Raise Error for Invalid AlbumID

--- app/classes/albums/sync.rb
+++ app/classes/albums/sync.rb
@@ -7,11 +7,15 @@ module Albums
     BASE_URL = 'jsonplaceholder.typicode.com'
 
     def self.call(album_id)
+      raise ArgumentError, 'Invalid Album ID' unless valid_id?(album_id)
+
       path = "/photos?albumId=#{album_id}"
-
       response = Net::HTTP.get_response(BASE_URL, path)
-
       JSON.parse(response.body)
+    end
+
+    def self.valid_id?(album_id)
+      album_id.is_a?(Integer)
     end
   end
 end
